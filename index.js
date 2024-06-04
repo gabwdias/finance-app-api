ed011741e268306696be5dadd9dcc30f4075f398
Gabriel Dias <gdias@atlassian.com>
2024-06-03 22:20:01 -0300

    feat: create postgres user create repository

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import { PostgresHelper } from './src/db/postgres/helper.js';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users;');
-    res.send(JSON.stringify(results));
-});
+// app.get('/', async (req, res) => {
+//     const results = await PostgresHelper.query('SELECT * FROM users;');
+//     res.send(JSON.stringify(results));
+// });
 
-app.listen(3000, () => console.log('App listening on port 3000'));
+app.listen(process.env.PORT, () =>
+    console.log(`App listening on port ${process.env.PORT}`)
+);
---
+++ src/repositories/postgres/create-user.js
@@ -0,0 +1,19 @@
+import { PostgresHelper } from '../../db/postgres/helper';
+
+export class PstegresCreateUserRepository {
+    async execute(createUserParams) {
+        //create the user in postgres
+        const results = await PostgresHelper.query(
+            'INSERT INTO users (ID, first_name, last_name, email, password) VALUES($1, $2, $3, $4, $5)',
+            [
+                createUserParams.ID,
+                createUserParams.first_name,
+                createUserParams.last_name,
+                createUserParams.email,
+                createUserParams.password,
+            ]
+        );
+
+        return results[0];
+    }
+}
