754c3dc83effa7a7d6cfd2ffd6876fb6127be6ba
Gabriel Dias <gdias@atlassian.com>
2024-06-05 21:16:49 -0300

    feat: added PostgresGetUserByIdRepository

---
+++ src/repositories/postgres/get-user-by-id.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from '../../db/postgres/helper';
+
+export class PostgresGetUserByIdRepository {
+    async execute(userId) {
+        const user = await PostgresHelper.query(
+            'SELECT * FROM users where id = $1',
+            [userId]
+        );
+
+        return user[0];
+    }
+}
