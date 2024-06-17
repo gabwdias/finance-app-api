70f4cced469b32fff142c930d01fcf7f57437d6e
Gabriel Dias <gdias@atlassian.com>
2024-06-17 20:29:04 -0300

    feat: added CreateTransactionController

---
+++ src/controllers/transaction/create-transaction.js
@@ -0,0 +1,83 @@
+import validator from 'validator';
+import {
+    badRequest,
+    checkIfIdIsValid,
+    created,
+    invalidIdResponse,
+    serverError,
+} from '../helpers';
+
+export class CreateTransactionController {
+    constructor(createTransactionUseCase) {
+        this.createTransactionUseCase = createTransactionUseCase;
+    }
+
+    async execute(httpRequestParams) {
+        try {
+            const params = httpRequestParams.body;
+
+            //validade mandatory fields
+            const requiredFields = [
+                'id',
+                'user_id',
+                'name',
+                'date',
+                'amount',
+                'type',
+            ];
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({ message: `Missing param: ${field}` });
+                }
+            }
+
+            //validate user is valid
+            const userIdIsValid = checkIfIdIsValid(params.userId);
+            if (!userIdIsValid) {
+                return invalidIdResponse();
+            }
+
+            //validate if amount is gt than 0
+            if (params.amount <= 0) {
+                return badRequest({
+                    message: 'The amount must be greater than 0',
+                });
+            }
+
+            const amountIsValid = validator.isCurrency(
+                params.amount.toString(),
+                {
+                    digits_after_decimal: [2],
+                    allow_negatives: false,
+                }
+            );
+
+            if (!amountIsValid) {
+                return badRequest({
+                    message: 'The amount must be a valid currency',
+                });
+            }
+
+            //validate if type is valide
+            const type = params.type.trim().toUpperCase();
+            const typeIsValid = ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(
+                type
+            );
+            if (!typeIsValid) {
+                return badRequest({
+                    message: 'The type must be EARNING, EXPENSE or INVESTMENT',
+                });
+            }
+
+            const transaction = await this.createTransactionUseCase.execute({
+                ...params,
+                type,
+            });
+            return created(transaction);
+        } catch (error) {
+            console.log(error);
+            return serverError();
+        }
+    }
+}
