79b2c770fc1730dbb9894eb121e545c6d6468a49
Gabriel Dias <gdias@atlassian.com>
2024-06-17 21:01:30 -0300

    feat: implemented the Create Transaction route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './src/factories/controllers/user.js';
+import { makeCreateTransactionController } from './src/factories/controllers/transaction.js';
 
 const app = express();
 app.use(express.json());
@@ -33,6 +34,13 @@ app.patch('/api/users/:userId', async (req, res) => {
 app.delete('/api/users/:userId', async (req, res) => {
     const deleteUserController = makeDeleteUserController();
     const { statusCode, body } = await deleteUserController.execute(req);
+
+    res.status(statusCode).send(body);
+});
+
+app.post('/api/transactions', async (req, res) => {
+    const createTransactionController = makeCreateTransactionController();
+    const { statusCode, body } = await createTransactionController.execute(req);
 
     res.status(statusCode).send(body);
 });
