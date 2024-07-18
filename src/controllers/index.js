6ac84edccea42b49dd29ea90fb497395f790aad7
Gabriel Dias <gdias@atlassian.com>
2024-07-18 17:46:32 -0300

    feat: add GetUserBalanceController

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ export * from './user/create-users.js';
 export * from './user/get-users-by-id.js';
 export * from './user/update-user.js';
 export * from './user/delete-user.js';
+export * from './user/get-user-balance.js';
 
 export * from './transaction/create-transaction.js';
 export * from './transaction/update-transaction.js';
